f7a8050be8a03d0ed05455ed732181223112ed2f
chengcnaplex <chengcn_aplex@aliyun.com>
2016-09-01 20:31:07 +0800

    first commit for EWT

---
+++ app/src/main/java/com/ateam/ewt/WelcomeUi.java
@@ -0,0 +1,13 @@
+package com.ateam.ewt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_welcome);
+    }
+}
